c750bce9aba64527a99bbc932f055f3295102634
VitorNunesCPT <vitornunescpt@gmail.com>
2023-06-01 12:09:33 -0300

    feat: add ListProducts

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -1,28 +1,19 @@
+import { Product } from '@prisma/client';
 import { prisma } from '../../prisma/client';
 import { CreateProductUseCaseDTO } from '../dtos/CreateProductUseDTO';
-import { Product } from '../models/product';
 
-export interface IProductRepository {
-  create({ name, description, price }: CreateProductUseCaseDTO): Promise<void>
-
-  list(): Promise<Product[]>
-}
+import { IProductRepository } from './IProductRepository';
 
 
-export class ProductRepository {
-  async create({ name, description, price }: CreateProductUseCaseDTO): Promise<Product> {
-    const product = prisma.product.create({ data: { name, description, price } })
-    return product
+
+export class ProductRepository implements IProductRepository{
+  async create({ name, description, price }: CreateProductUseCaseDTO): Promise<void> {
+    await prisma.product.create({ data: { name, description, price } })
   }
 
   async list(): Promise<Product[]> {
-    const products = prisma.product.findMany()
+    const products = await prisma.product.findMany()
     return products
-  }
-
-  async findByName(name: string): Promise<Product | null> {
-    const product = prisma.product.findFirst({ where: { name } })
-    return product
   }
 
 }
---
+++ src/useCases/ListProductsUseCase/ListProductsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ListProductsUseCase } from "./ListProductsUseCase";
+
+
+
+// Handle convenção 
+export class ListProductsController {
+    constructor(private readonly listProductsUseCase:ListProductsUseCase){}
+    async handle(request:Request, response:Response){
+        const products = await this.listProductsUseCase.execute()
+        return response.status(201).json(products)
+    }
+
+}
